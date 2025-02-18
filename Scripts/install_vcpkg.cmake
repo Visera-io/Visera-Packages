a656e43ee803a35e3b0ed3fe21043b78e2ede9b5
LJYC <ljyc.me@gmail.com>
2025-02-18 19:18:34 +0900

    Init

--- Scripts/install_vcpkg.cmake
+++ Scripts/install_vcpkg.cmake
@@ -14,20 +14,20 @@ if (NOT DEFINED VISERA_PACKAGES_VCPKG_EXE)
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     )
 
-    if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
         set(VISERA_PACKAGES_VCPKG_EXE "${VISERA_PACKAGES_VCPKG_DIR}/vcpkg.exe" CACHE PATH "")
         execute_process(
             COMMAND bootstrap-vcpkg.bat
             WORKING_DIRECTORY ${VISERA_PACKAGES_VCPKG_DIR}
         )
-    elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux" OR CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
         set(VISERA_PACKAGES_VCPKG_EXE "${VISERA_PACKAGES_VCPKG_DIR}/vcpkg" CACHE PATH "")
         execute_process(
             COMMAND bootstrap-vcpkg.sh
             WORKING_DIRECTORY ${VISERA_PACKAGES_VCPKG_DIR}
         )
     else()
-        message(FATAL_ERROR "Unknown platform, script cannot be executed.")
+        message(FATAL_ERROR "Unknown platform \"${CMAKE_HOST_SYSTEM_NAME}\", script cannot be executed.")
     endif()
 else()
     message(NOTICE "Vcpkg has been installed but may not up to date.")
