b0f21a38b3f072e5f041498f62b4a89976f9f9f5
LJYC <ljyc.me@gmail.com>
2025-03-06 21:07:41 +0800

    Update

--- Scripts/install_slang.cmake
+++ Scripts/install_slang.cmake
@@ -10,9 +10,23 @@ macro(link_slang _target)
     message(STATUS "\nLoading Slang (slang)")
     # Make sure that Version(VulkanSDK) >= "1.4.0"
     add_library(slang UNKNOWN IMPORTED)
-    set_target_properties(slang
-        PROPERTIES
-        IMPORTED_LOCATION "$ENV{VULKAN_SDK}/Lib/slang.lib"
-        INTERFACE_INCLUDE_DIRECTORIES "$ENV{VULKAN_SDK}/Include/slang")
-    target_link_libraries(${_target} PUBLIC slang)
+    if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+        set_target_properties(slang
+                PROPERTIES
+                IMPORTED_LOCATION "$ENV{VULKAN_SDK}/Lib/slang.lib"
+                INTERFACE_INCLUDE_DIRECTORIES "$ENV{VULKAN_SDK}/Include/slang")
+        target_link_libraries(${_target} PUBLIC slang)
+
+    elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin") # MacOS
+        set_target_properties(slang
+                PROPERTIES
+                IMPORTED_LOCATION "$ENV{VULKAN_SDK}/lib/libslang.dylib"
+                INTERFACE_INCLUDE_DIRECTORIES "$ENV{VULKAN_SDK}/include/slang")
+        target_link_libraries(${_target} PUBLIC slang)
+
+    #elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    else()
+        message(FATAL_ERROR "Unsupported Platform!")
+    endif()
+
 endmacro()
