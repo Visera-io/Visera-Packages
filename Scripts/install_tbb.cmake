7573e6379a396062eaa90de51a44cb1afbbf5ef2
LJYC <ljyc.me@gmail.com>
2025-03-27 23:14:18 +0900

    TBB

--- Scripts/install_tbb.cmake
+++ Scripts/install_tbb.cmake
@@ -2,40 +2,16 @@ set(TBB_VERSION "2022.0.0")
 set(TBB_CACHE_PATH   "${VISERA_PACKAGES_CACHE_DIR}")
 set(TBB_INSTALL_PATH "${VISERA_PACKAGES_INSTALL_DIR}/TBB")
 
-add_custom_target(TBB)
-target_sources(TBB PRIVATE "${VISERA_PACKAGES_SCRIPTS_DIR}/install_tbb.cmake")
-set_target_properties(TBB PROPERTIES FOLDER "Visera/Packages/TBB")
+add_custom_target(OneTBB)
+target_sources(OneTBB PRIVATE "${VISERA_PACKAGES_SCRIPTS_DIR}/install_tbb.cmake")
+set_target_properties(OneTBB PROPERTIES FOLDER "Visera/Packages/TBB")
 
-# Check if installed
-if (NOT EXISTS "${TBB_INSTALL_PATH}/oneapi-tbb-${TBB_VERSION}/lib/cmake/tbb")
-    message(STATUS "Installing TBB${TBB_VERSION} (TBB)...")
-    # Check if exist cache
-    if (WIN32)
-        set(TBB_CACHE "${TBB_CACHE_PATH}/oneapi-tbb-${TBB_VERSION}-win.zip")
-    elseif (APPLE) # MacOS
-        set(TBB_CACHE "${TBB_CACHE_PATH}/oneapi-tbb-${TBB_VERSION}-mac.tgz")
-    elseif (LINUX)
-        set(TBB_CACHE "${TBB_CACHE_PATH}/oneapi-tbb-${TBB_VERSION}-lin.tgz")
-    else()
-        message(FATAL_ERROR "Unsupported Platform!")
-    endif()
-
-    if (NOT DEFINED TBB_CACHE)
-        message(FATAL_ERROR "Cache is not found and start downloading... (WIP)")
-        #set(TBB_URL "https://github.com/uxlfoundation/oneTBB/releases/download/v${TBB_VERSION}/oneapi-tbb-${TBB_VERSION}-win.zip")
-        #file(DOWNLOAD ${TBB_URL} ${TBB_CACHE_PATH} SHOW_PROGRESS)
-    endif()
-
-    message(STATUS "Found TBB${TBB_VERSION} Cache: ${TBB_CACHE}")
-    file(ARCHIVE_EXTRACT
-        INPUT ${TBB_CACHE}
-        DESTINATION ${TBB_INSTALL_PATH})
-endif()
+execute_process(COMMAND ${VISERA_PACKAGES_VCPKG_EXE} install tbb)
 
 macro(link_tbb _target)
-    message(STATUS "Loading TBB${TBB_VERSION} (TBB)...")
+    message(STATUS "Loading Intel OneTBB${TBB_VERSION} (TBB::tbb)...")
 
-    set(TBB_DIR "${TBB_INSTALL_PATH}/oneapi-tbb-${TBB_VERSION}/lib/cmake/tbb")
+    #set(TBB_DIR "${TBB_INSTALL_PATH}/oneapi-tbb-${TBB_VERSION}/lib/cmake/tbb")
     find_package(TBB REQUIRED)
 
     target_link_libraries(${_target}
