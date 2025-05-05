f7923a00f6845849dbc1aff35daac39feb8274df
LJYC <ljyc.me@gmail.com>
2025-05-05 22:24:50 +0900

    RapidJSON
    MacOS

--- Scripts/install_rapidjson.cmake
+++ Scripts/install_rapidjson.cmake
@@ -4,8 +4,9 @@ if(NOT TARGET RapidJSON)
     message(STATUS "Installing RapidJSON(${RAPIDJSON_VERSION}) (rapidjson)...")
 
     set(VISERA_PACKAGES_RAPIDJSON_SOURCE_DIR "${VISERA_PACKAGES_SOURCE_DIR}/RapidJSON")
-    file(GLOB_RECURSE VISERA_PACKAGES_RAPIDJSON_SOURCE_FILES "${VISERA_PACKAGES_RAPIDJSON_SOURCE_DIR}/*")
-    
+    #file(GLOB_RECURSE VISERA_PACKAGES_RAPIDJSON_SOURCE_FILES "${VISERA_PACKAGES_RAPIDJSON_SOURCE_DIR}/*")
+    execute_process(COMMAND ${VISERA_PACKAGES_VCPKG_EXE} install rapidjson)
+
     add_custom_target(RapidJSON)
     target_sources(RapidJSON PRIVATE "${VISERA_PACKAGES_SCRIPTS_DIR}/install_rapidjson.cmake")
     set_target_properties(RapidJSON PROPERTIES FOLDER "Visera/Packages/RapidJSON")
@@ -13,5 +14,6 @@ endif()
 
 macro(link_rapidjson _target)
     message(STATUS "\nLoading Rapid JSON (rapidjson)")
-    target_include_directories(${_target} PUBLIC ${VISERA_PACKAGES_RAPIDJSON_SOURCE_DIR})
+    find_package(RapidJSON CONFIG REQUIRED)
+    target_link_libraries(${_target} PUBLIC rapidjson)
 endmacro()
